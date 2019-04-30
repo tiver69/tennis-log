cf2459f669d79c5bec4aa14d4d66c2feef4b056a
Aleksandra <kibko.sasha@gmail.com>
2019-04-30 12:10:22 +0300

    generate empty project

---
+++ back/src/main/java/org/dss/tennislog/TennisLogApplication.java
@@ -0,0 +1,13 @@
+package org.dss.tennislog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisLogApplication.class, args);
+	}
+
+}
