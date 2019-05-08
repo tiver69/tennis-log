fdd833cea4e76b598ef374df552ecd8384c7c50b
Aleksandra <kibko.sasha@gmail.com>
2019-05-09 02:07:37 +0300

    update footer

--- react-client/src/components/Layout/Footer.js
+++ react-client/src/components/Layout/Footer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
 	render(){
 		return (
-			<div>
-				<h1>footer</h1>
-			</div>
+			<footer className="page-footer font-small">
+				<div className="footer-copyright text-center py-3">© 2019 Copyright:{" "}
+				    <Link to="http://dss-tennis.com.ua/">dss-tennis.com.ua</Link>
+				</div>
+			</footer>
 		);
 	}
 }
